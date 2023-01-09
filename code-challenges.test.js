ca1a2408d86b5681c1737c7ca386988203004c0b
chrisyhlee <leechr8393@gmail.com>
2023-01-08 18:05:57 -0800

    completed assessment week 4

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,9 @@
 // ASSESSMENT 4: JavaScript Coding Practical Questions with Jest
 
+const { notStrictEqual } = require("assert")
+const { join } = require("path")
+const { boolean } = require("yargs")
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -15,6 +19,7 @@
 // a) Create a test with an expect statement using the variable provided.
 // HINT: Check out this resource: https://jestjs.io/docs/expect#expectarraycontainingarray
 
+
 const colors1 = ["purple", "blue", "green", "yellow", "pink"]
 // Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
 const colors2 = [
@@ -27,25 +32,135 @@ const colors2 = [
 ]
 // Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
 
+// PSEUDOCODE
+// create fxn called randomizer that takes in an array and removes the first item, then shuffles the remaining contents
+// input: array
+// output: array without first item, shuffled
+// use slice to remove first index of received array
+// loop through array from back to front
+// assign random number to variable
+// assign current index to random number index
+
+describe ('randomizer', () => {
+  const expectedArr = ['yellow', 'blue', 'pink', 'green']
+  const expectedArr2 = ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
+  it ('takes an array, removes the first item, then shuffles the remaining content', () => {
+    expect (randomizer(colors1)).toEqual(expect.arrayContaining(expectedArr))
+    expect (randomizer(colors2)).toEqual(expect.arrayContaining(expectedArr2))
+  })
+})
+
+// FAIL  ./code-challenges.test.js
+// randomizer
+//   ✕ takes an array, removes the first item, then shuffles the remaining content (1 ms)
+
+// ● randomizer › takes an array, removes the first item, then shuffles the remaining content
+
+//   ReferenceError: randomizer is not defined
+
 // b) Create the function that makes the test pass.
+
+const randomizer = (arr) => {
+  slcArr = arr.slice(1, arr.length)
+  for (let i = slcArr.length -1; i > 0; i--) {
+    const j = Math.floor(Math.random() * slcArr.length)
+    const k = slcArr[i]
+    slcArr[i] = slcArr[j]
+    slcArr[j] = k
+  }
+  return slcArr
+}
+// console.log(randomizer(colors2));
+
+// PASS  ./code-challenges.test.js
+// randomizer
+//   ✓ takes an array, removes the first item, then shuffles the remaining content (1 ms)
 
 // --------------------2) Create a function that takes in an object that contains up votes and down votes and returns the end tally.
 
 // a) Create a test with expect statements for each of the variables provided.
+
+// PSEUDOCODE
+// create fxn that takes an object and returns end tally of votes
+// input: object
+// output: end tally (number)
+// use dot notation to access key value pairs within objects
+// subtract downvotes from upvotes
+
 
 const votes1 = { upVotes: 13, downVotes: 2 }
 // Expected output: 11
 const votes2 = { upVotes: 2, downVotes: 33 }
 // Expected output: -31
 
+describe ('endTally', () => {
+  it ('takes in an object containing up and downvotes and returns the end tally', () => {
+    expect (endTally(votes1)).toEqual(11)
+    expect (endTally(votes2)).toEqual(-31)
+  })
+})
+
+// FAIL  ./code-challenges.test.js
+// endTally
+//   ✕ takes in an object containing up and downvotes and returns the end tally (1 ms)
+
+// ● endTally › takes in an object containing up and downvotes and returns the end tally
+
+//   ReferenceError: endTally is not defined
+
 // b) Create the function that makes the test pass.
+
+const endTally = (obj) => {
+  return obj.upVotes - obj.downVotes
+}
+// PASS  ./code-challenges.test.js
+// endTally
+//   ✓ takes in an object containing up and downvotes and returns the end tally (1 ms)
 
 // --------------------3) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
 
 // a) Create a test with an expect statement using the variables provided.
 
+// PSEUDOCODE
+// declare fxn called noDupes that takes in 2 arrays and returns only 1 array with no duplicates.
+// input: arrays
+// output: array with no duplicates
+// use spread operator to make the number of arguments the fxn can take dynamic
+// use forEach on argument, then forEach on each value of the argument and push anything that the empty array does not already include into the empty array variable
+
 const dataArray1 = ["array", "object", "number", "string", "Boolean"]
 const dataArray2 = ["string", "null", "Boolean", "string", "undefined"]
 // Expected output: ["array", "object", "number", "string", "Boolean", "null", "undefined"]
 
+describe ('noDupes', () => {
+  it ('takes in two arrays and returns one without duplicates', () => {
+    expect (noDupes(dataArray1, dataArray2)).toEqual(expect.arrayContaining(["array", "object", "number", "string", "Boolean", "null", "undefined"]))
+  })
+})
+// FAIL  ./code-challenges.test.js
+// noDupes
+//   ✕ takes in two arrays and returns one without duplicates
+
+// ● noDupes › takes in two arrays and returns one without duplicates
+
+//   ReferenceError: noDupes is not defined
+
 // b) Create the function that makes the test pass.
+
+const noDupes = (...arrs) => {
+  let noDupeArr = []
+  arrs.forEach((arrValue) => {
+    arrValue.forEach((value) => {
+      if (!noDupeArr.includes(value)) {
+        noDupeArr.push(value)
+      }
+    })
+  })
+  return noDupeArr
+}
+
+// console.log(noDupes(dataArray1, dataArray2));
+
+// PASS  ./code-challenges.test.js
+// noDupes
+//   ✓ takes in two arrays and returns one without duplicates
--- interview-questions.md
+++ interview-questions.md
@@ -8,42 +8,47 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is object-oriented programming? How is it different than functional programming?
 
-Your answer:
+Your answer: Object-oriented programming, AKA OOP, is a type of programming in which everything is an object, coming from a class. For example, in Ruby, the number 15 is considered to be an object, coming from the class of Integer.
 
-Researched answer:
+Researched answer: Object-oriented programming is a programming model that revolves around objects instead of functions and logic. In OOP, everything is an object, which means that everything is an instance of a class.
 
 1. What is the difference between a Float and an Integer in Ruby?
 
-Your answer:
+Your answer: A Float is a class that lays out objects of partial numbers. An Integer is a class that lays out objects of whole numbers. An example of an Integer would be 17, while an example of a Float would be 17.0 or 17.6. While using irb in terminal, in order to receive an output of a Float, you must first introduce a Float to the initial input. For example, an input of 5/2 would output 2, whereas an input of 5/2.5 would then output 2.5.
 
-Researched answer:
+Researched answer: An Integer is a whole number whereas a Float is a partial number. Examples are 2 and 2.5, respectively.
 
 1. Ruby has an implicit return. What does that mean?
 
-Your answer:
+Your answer: While not 100% sure, I believe that implicit return means that the return statement is implied within Ruby. For example, if I was creating a method using def and end, in between the def and end i could write the code:
+'Hello There!'
+without an actual 'return' keyword. When I perform a method call on that method, it would automatically return 'Hello There!' even though I did not code in a return keyword.
 
-Researched answer:
+Researched answer: Implicit return means that there is no need to use the word return in order to get a return in the output of code. As long as a return is the last expression in the code path of a given piece of code, implicit return will automatically have that return in the output even without using return.
 
 1. What is a block in Ruby?
 
-Your answer:
+Your answer: A block is an anonymous function that is used with a method. Blocks use the "do and end" keywords or curly braces. An example of a block would be if I wanted to use the ".times" method to print 'hello' 3 times, I would use the code:
+3.times do
+    p 'hello'
+end
 
-Researched answer:
+Researched answer: A block is defined as a little anonymous function that can be passed into methods. Blocks start and end with do and end statements, but can also be enclosed within code brackets {}.
 
 1. How do you extract a value in a Ruby hash?
 
-Your answer:
+Your answer: You can extract a value in a Ruby hash by using .values on the hash to get all of the values within that hash. If you want a specific value, you can use bracket notation to enclose the specific key you want the value of. For example, from the hash called example_hash = {:today=>"Workout day", :tomorrow=>"Rest day"}, we could print all the values using example_hash.values or only the value of today using example_hash[:today].
 
-Researched answer:
+Researched answer: Extract a value in a Ruby hash using bracket notation with the key inside. An example would be name_of_hash[:key_name].
 
 ## Looking Ahead: Terms for Next Week
 
-1. Class Inheritance:
+1. Class Inheritance: Class inheritance is a mechanism that allows the programmer to make one class adopt the characteristics of a different class.
 
-2. RSpec:
+2. RSpec: A testing framework for Ruby, similar to jest for Javascript.
 
-3. CRUD:
+3. CRUD: Stand for create, read, update, delete. An application is considered to be a full-stack application when all parts of CRUD are fulfilled and operational.
 
-4. Test-driven development:
+4. Test-driven development: A process in which software development is repeatedly tested before being fully developed.
 
-5. HTTP:
+5. HTTP: Stands for hypertext transfer protocol. It is used for communication between browsers and servers, and is the foundation of any data exchange on the Web.
